6458ef53bb69c091bcbe5427b60aaf224ecf5e29
Nufore <nufore@nufore.nu>
2025-06-25 17:12:38 +0300

    LimitedSizeStack

---
+++ LimitedSizeStack/LimitedSizeStack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LimitedSizeStack;
+
+public class LimitedSizeStack<T>
+{
+    public LinkedList<T> Items { get; }
+    public int UndoLimit;
+
+    public LimitedSizeStack(int undoLimit)
+    {
+        Items = new LinkedList<T>();
+        UndoLimit = undoLimit;
+    }
+
+    public void Push(T item)
+    {
+        if (UndoLimit == 0)
+            return;
+        if (Items.Count == UndoLimit)
+            Items.RemoveFirst();
+        Items.AddLast(item);
+    }
+
+    public T Pop()
+    {
+        if (Items.Count == 0) throw new InvalidOperationException();
+        var result = Items.Last.Value;
+        Items.RemoveLast();
+        return result;
+    }
+
+    public int Count => Items.Count;
+}
