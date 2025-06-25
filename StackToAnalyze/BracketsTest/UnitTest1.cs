9744c515e4141d5020197a995dd02c6967495e85
MacBook Air <macbookair@MacBook-Air-MacBook.local>
2025-06-25 13:45:58 +0300

    Stack to analyze brackets with tests

---
+++ StackToAnalyze/BracketsTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using StackToAnalyze;
+namespace BracketsTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(Program.IsCorrectString("(([[]]))"));
+    }
+}
---
+++ StackToAnalyze/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace StackToAnalyze;
+
+public class Program
+{
+
+    public static bool IsCorrectString(string str)
+    {
+        var stack = new Stack<char>();
+        foreach (var symbol in str)
+        {
+            char openBraket;
+            switch (symbol)
+            {
+                case '(':
+                case '[':
+                    stack.Push(symbol);
+                    break;
+                case ')':
+                    if (stack.Count == 0) return false;
+                    openBraket = stack.Pop();
+                    if (openBraket != '(') return false;
+                    break;
+                case ']':
+                    if (stack.Count == 0) return false;
+                    openBraket = stack.Pop();
+                    if (openBraket != '[') return false;
+                    break;
+            }
+        }
+        return stack.Count == 0;
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsCorrectString("(([])[])"));
+        Console.WriteLine(IsCorrectString("((][])"));
+        Console.WriteLine(IsCorrectString("((("));
+        Console.WriteLine(IsCorrectString("(x)"));
+    }
+}
