1e37d89721c6f4189f3bf7fa393f362b8b166d2b
Nufore <nufore@nufore.nu>
2025-06-25 17:11:56 +0300

    MaxInArray

---
+++ MaxInArray/MaxInArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace MaxInArray;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(Max(new int[0]));
+        Console.WriteLine(Max(new[] { 3 }));
+        Console.WriteLine(Max(new[] { 3, 1, 2 }));
+        Console.WriteLine(Max(new[] { "A", "B", "C" }));
+    }
+
+    static T Max<T>(T[] source) where T : IComparable<T>
+    {
+        if (source.Length == 0)
+            return default(T);
+
+        T max = source[0];
+        for (int i = 1; i < source.Length; i++)
+        {
+            if (source[i].CompareTo(max) > 0)
+            {
+                max = source[i];
+            }
+        }
+
+        return max;
+    }
+}
